69ed6602ab107c77776146bd27d46620ccb124f5
Matthew Lawrence <Mattplawrence@gmail.com>
2022-01-25 15:47:05 -0500

    connecting index.html to notes.html

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+
+const PORT = process.env.PORT || 3002;
+
+const app = express();
+
+app.use(express.static("public"));
+
+//Get landing page
+// app.use("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "/public/index.html"));
+// });
+app.use("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+app.listen(PORT, () => console.log("App listening on port " + PORT));
